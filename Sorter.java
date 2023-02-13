6191d139c8ba3fe286e5f5dcfc00395b6ad4200c
Muath Alghamdi <Muath2312@gmail.com>
2023-02-13 16:32:44 +0300

    Muath insertion sort done

--- Sorter.java
+++ Sorter.java
@@ -3,7 +3,7 @@
 import java.io.*;
 
 public class Sorter {
-    void sort(int arr[]) {
+    void selectionSort(int arr[]) {
         int n = arr.length;
 
         // One by one move boundary of unsorted subarray
@@ -21,9 +21,25 @@ public class Sorter {
             arr[i] = temp;
         }
     }
+    void insertionsort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
 
+			/* Move elements of arr[0..i-1], that are
+			greater than key, to one position ahead
+			of their current position */
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
     // Prints the array
-    void printArray(int arr[]) {
+    static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
@@ -31,12 +47,34 @@ public class Sorter {
     }
 
     // Driver code to test above
+
+    // Java program for implementation of Insertion Sort
+
+	/*Function to sort array using insertion sort*/
+	
+
+	/* A utility function to print array of size n*/
+	
+
+	// Driver method
+	
+
+
+
+/* This code is contributed by Rajat Mishra. */
+
     public static void main(String args[]) {
         Sorter ob = new Sorter();
         int arr[] = { 64, 25, 12, 22, 11 };
-        ob.sort(arr);
+        ob.selectionSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
+        int arr1[] = { 12, 11, 13, 5, 6 };
+
+		Sorter ob1 = new Sorter();
+		ob1.insertionsort(arr1);
+
+		printArray(arr1);
     }
 }
 /* This code is contributed by Rajat Mishra */
