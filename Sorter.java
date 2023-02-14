d592a125d3ee5d01f60a279dc7f175ea2436509e
Feras Alhazmi <feras.alhazmy@gmail.com>
2023-02-14 20:27:05 +0300

    fixed conflicts and used slectionsort

--- Sorter.java
+++ Sorter.java
@@ -3,7 +3,7 @@
 import java.io.*;
 
 public class Sorter {
-    void selectionSort(int arr[]) {
+    static void selectionSort(int arr[]) {
         int n = arr.length;
 
         // One by one move boundary of unsorted subarray
@@ -21,23 +21,26 @@ public class Sorter {
             arr[i] = temp;
         }
     }
-    static void insertionsort(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 1; i < n; ++i) {
-			int key = arr[i];
-			int j = i - 1;
 
-			/* Move elements of arr[0..i-1], that are
-			greater than key, to one position ahead
-			of their current position */
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
-		}
-	}
+    static void insertionsort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /*
+             * Move elements of arr[0..i-1], that are
+             * greater than key, to one position ahead
+             * of their current position
+             */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
     // Prints the array
     static void printArray(int arr[]) {
         int n = arr.length;
@@ -46,27 +49,26 @@ public class Sorter {
         System.out.println();
     }
 
-    
     public static void main(String args[]) {
         Sorter ob = new Sorter();
         int arr[] = { 64, 25, 12, 22, 11 };
-        //ob.selectionSort(arr);
+        // ob.selectionSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
         int arr1[] = { 12, 11, 13, 5, 6 };
 
-		Sorter ob1 = new Sorter();
-		ob1.insertionsort(arr1);
+        Sorter ob1 = new Sorter();
+        ob1.insertionsort(arr1);
         sort(arr);
         System.out.println("TEST");
         printArray(arr);
 
-		printArray(arr1);
-
+        printArray(arr1);
 
     }
-    private static void sort(int [] arr) {
-        insertionsort(arr);
+
+    private static void sort(int[] arr) {
+        selectionSort(arr);
+        // last edited computer A feras alhazmi
     }
 }
-/* This code is contributed by Rajat Mishra */
